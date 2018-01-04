45c206ae06fab606f27aa4d3cff3d1263da45640
dangkhoaicao <dangkhoaicao@gmail.com>
2018-01-04 12:54:05 +0700

    change password heroku

--- ecosystem.config.heroku.js
+++ ecosystem.config.heroku.js
@@ -10,7 +10,7 @@ module.exports = {
         DATABASE_ADDRESS: 'us-cdbr-iron-east-05.cleardb.net',
         DATABASE_NAME: 'heroku_9fa687cf85aeae2',
         DATABASE_USER: 'b05a91b9342c38',
-        DATABASE_PASSWORD: 'f71d30c3',
+        DATABASE_PASSWORD: 'd4526772fd814fc',
         SALT: 10
       },
       env_production : {
