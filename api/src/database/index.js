75988a6fb76fe664320cad85c12059502b93b173
dangkhoaicao <dangkhoaicao@gmail.com>
2018-01-04 13:57:30 +0700

    fix heroku config bug

--- api/src/database/index.js
+++ api/src/database/index.js
@@ -4,7 +4,7 @@ const tables = require('./tables');
 const database = process.env.DATABASE_NAME;
 const username = process.env.DATABASE_USER;
 const password = process.env.DATABASE_PASSWORD;
-const host = process.env.DATABASE_ADRRESS;
+const host = process.env.DATABASE_ADDRESS;
 
 const sequelize = new Sequelize(database, username, password, {
     host: host,
--- ecosystem.heroku.config.js
+++ ecosystem.heroku.config.js
@@ -1,8 +1,7 @@
 module.exports = {
- 
   apps : [
     {
-      name      : 'API',
+      name      : 'NODECLOBEN_API',
       script    : 'api/server.js',
       watch: true,
       env: {
@@ -14,7 +13,13 @@ module.exports = {
         SALT: 10
       },
       env_production : {
-        NODE_ENV: 'production'
+        NODE_ENV: 'production',
+        PORT: 4040,
+        DATABASE_ADDRESS: 'us-cdbr-iron-east-05.cleardb.net',
+        DATABASE_NAME: 'heroku_9fa687cf85aeae2',
+        DATABASE_USER: 'b05a91b9342c38',
+        DATABASE_PASSWORD: 'd4526772fd814fc',
+        SALT: 10
       }
     }
   ],
