0c6339c4935ef30faece86e43c695027c7455625
dangkhoaicao <dangkhoaicao@gmail.com>
2018-01-04 16:18:43 +0700

    add config to production

--- ecosystem.heroku.config.js
+++ ecosystem.heroku.config.js
@@ -14,6 +14,12 @@ module.exports = {
       },
       env_production : {
         NODE_ENV: 'production',
+        PORT: 4040,
+        DATABASE_ADDRESS: 'us-cdbr-iron-east-05.cleardb.net',
+        DATABASE_NAME: 'heroku_9fa687cf85aeae2',
+        DATABASE_USER: 'b05a91b9342c38',
+        DATABASE_PASSWORD: 'd4526772fd814fc',
+        SALT: 10
       }
     }
   ]
