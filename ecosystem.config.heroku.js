d39f7fc55218c1fc28184c4141f351f6580f3258
dangkhoaicao <dangkhoaicao@gmail.com>
2018-01-04 12:00:32 +0700

    add Heroku and some config

---
+++ ecosystem.config.heroku.js
@@ -0,0 +1,43 @@
+module.exports = {
+ 
+  apps : [
+    {
+      name      : 'API',
+      script    : 'api/server.js',
+      watch: true,
+      env: {
+        PORT: 4040,
+        DATABASE_ADDRESS: 'us-cdbr-iron-east-05.cleardb.net',
+        DATABASE_NAME: 'heroku_9fa687cf85aeae2',
+        DATABASE_USER: 'b05a91b9342c38',
+        DATABASE_PASSWORD: 'f71d30c3',
+        SALT: 10
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/development',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
