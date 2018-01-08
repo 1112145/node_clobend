116f50b9aa51d9539ae4315321e447f921a04955
dangkhoaicao <dangkhoaicao@gmail.com>
2018-01-08 09:46:16 +0700

    update environment

--- ecosystem.config.template.js
+++ ecosystem.config.template.js
@@ -10,7 +10,9 @@ module.exports = {
       DATABASE_NAME: 'DATABASE_NAME',
       DATABASE_USER: 'DATABASE_USER',
       DATABASE_PASSWORD: 'DATABASE_PASSWORD',
-      SALT: 'SALT_NUMBER'
+      SALT: 'SALT_NUMBER',
+      FACEBOOK_APP_ID: 'FACEBOOK_APP_ID',
+      FACEBOOK_APP_SECRET: 'FACEBOOK_APP_SECRET'
     },
     env_production: {
       NODE_ENV: 'production'
--- ecosystem.heroku.config.js
+++ ecosystem.heroku.config.js
@@ -9,6 +9,8 @@ module.exports = {
         DATABASE_NAME: 'heroku_9fa687cf85aeae2',
         DATABASE_USER: 'b05a91b9342c38',
         DATABASE_PASSWORD: 'd4526772fd814fc',
+        FACEBOOK_APP_ID: '141852669858737',
+        FACEBOOK_APP_SECRET: '4f1dec7f1346f2ca19749bc5a6820e1a',
         SALT: 10
       },
       env_production : {
